49ea6c56498fcc3220a3d84a0206186181e05820
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-19 00:53:09 +0100

    used 3 methods for destructuring array

--- countries.js
+++ countries.js
@@ -11,11 +11,35 @@ const displayCountries = countries => {
     countryContainer.innerHTML = countriesHTML.join(' ')
 }
 
-const getCountryHTML = country => {
+// option1 (get country data)
+// const getCountryHTML = country => {
+//     return `
+//     <div class = "country">
+//         <h3>${country.name}</h3>
+//         <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
+
+// option2(get country data)
+// const getCountryHTML = country => {
+//     const { name, flags } = country;
+//     return `
+//     <div class = "country">
+//         <h3>${name}</h3>
+//         <img src="${flags.png}">
+//     </div>
+//     `
+// }
+
+// option3(get country data)
+const getCountryHTML = ({ name, flags, capital, population }) => {
     return `
     <div class = "country">
-        <h3>${country.name}</h3>
-        <img src="${country.flags.png}">
+        <h3>${name}</h3>
+        <h4>${capital}</h4>
+        <h5>${population}</h5>
+        <img src="${flags.png}">
     </div>
     `
 }
