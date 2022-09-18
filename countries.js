589554d0519b031e83583d979f6ab6b5b83764f8
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-18 23:28:20 +0100

    countries info added

--- countries.js
+++ countries.js
@@ -1,7 +1,22 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v2/all')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayCountries(data))
+}
+const displayCountries = countries => {
+    console.log(countries[0])
+    const countriesHTML = countries.map(country => getCountryHTML(country))
+    // console.log(countriesHTML[0])
+    const countryContainer = document.getElementById('country')
+    countryContainer.innerHTML = countriesHTML.join(' ')
 }
 
+const getCountryHTML = country => {
+    return `
+    <div class = "country">
+        <h3>${country.name}</h3>
+        <img src="${country.flags.png}">
+    </div>
+    `
+}
 loadCountries()
